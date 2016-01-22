645e2aa9a5418b4abdcf17375bd2d1ef5f87856a
Alex Hardwicke <alex.hardwicke@outlook.com>
2016-01-22 08:00:45 +0100

    Change browser to Chrome
    FF doesn't seem to work wonderfully with Elementary OS. :(

--- setup.sh
+++ setup.sh
@@ -129,7 +129,7 @@ then
     gsettings set org.pantheon.terminal.settings opacity '100'
     gsettings set org.gnome.desktop.interface monospace-font-name 'Powerline Consolas 15'
     gsettings set com.canonical.indicator.datetime time-format '12-hour'
-    printf "\nMANUAL CONFIGURATION:\nSet default web browser to Firefox\nSet Language to en-GB\nDownload language pack\nSet trackpad speed\nDisable Guest\nDisable second monitor\nUnpin calendar, music, video, photos\nPin terminal, firefox\n\n"
+    printf "\nMANUAL CONFIGURATION:\nInstall Chrome and set it as the default web browser\nSet Language to en-GB\nDownload language pack\nSet trackpad speed\nDisable Guest\nDisable second monitor\nUnpin calendar, music, video, photos\nPin terminal, Chrome\n\n"
 else
     printf "\nMANUAL CONFIGURATION:\nAdd bashconf/keychain-2.8.1 to path\nInstall Consolas font\n\n"
 fi
