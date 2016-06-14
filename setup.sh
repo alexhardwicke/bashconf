12d0daba38f0b843384ea7d75ce27aab86699903
Alex Hardwicke <alex.hardwicke@outlook.com>
2016-06-14 13:43:19 +0200

    Remove old diff-so-fancy

--- setup.sh
+++ setup.sh
@@ -130,12 +130,7 @@ then
     fi
     cd $cwd
     cd bin
-    cp diff-so-fancy ~/bin/
-    cp -r third_party ~/bin/
-    cp -r libs ~/bin/
     chmod +x ~/bin/diff-so-fancy
-    chmod +x ~/bin/libs/header_clean/header_clean.pl
-    chmod +x ~/bin/third_party/diff-highlight/diff-highlight
 fi
 
 # Set up cowsay
